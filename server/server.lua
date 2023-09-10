c3f4b0c4f40b925b9b0e7669711117ab438b30a6
Muhammad Is'ad Prabaswara <81808543+ovdsd@users.noreply.github.com>
2023-09-10 22:55:54 +0700

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -1,18 +1,96 @@
 -----------------For support, scripts, and more----------------
 --------------- https://discord.gg/wasabiscripts  -------------
 ---------------------------------------------------------------
+ESX = exports['es_extended']:getSharedObject()
+local swapHook, buyHook
 
-ESX = exports["es_extended"]:getSharedObject()
-
-lib.callback.register('ws_sellshop:sellItem', function(source, data)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local xItem = xPlayer.getInventoryItem(data.item)
-    if xItem.count < data.quantity then
-        return false
-    else
-        local profit = math.floor(data.price * data.quantity)
-        xPlayer.removeInventoryItem(data.item, data.quantity)
-        xPlayer.addAccountMoney(data.currency, profit)
-        return profit
+CreateThread(function()
+    while ESX == nil do Wait() end
+    for k,_ in pairs(TokoCFG.Shops) do
+        TriggerEvent('esx_society:registerSociety', k, k, 'society_'..k, 'society_'..k, 'society_'..k, {type = 'public'})
     end
+end) 
+
+CreateThread(function()
+	while GetResourceState('ox_inventory') ~= 'started' do Wait(1000) end
+	for k,v in pairs(TokoCFG.Shops) do
+		local stash = {
+			id = k,
+			label = v.label..' '..Strings.inventory,
+			slots = 50,
+			weight = 100000,
+		}
+		exports.ox_inventory:RegisterStash(stash.id, stash.label, stash.slots, stash.weight)
+		local items = exports.ox_inventory:GetInventoryItems(k, false)
+		local stashItems = {}
+		if items and items ~= {} then
+			for k,v in pairs(items) do
+				if v and v.name then
+					stashItems[#stashItems + 1] = { name = v.name, metadata = v.metadata, count = v.count, price = (v.metadata.shopData.price or 0) }
+				end
+			end
+			local x,y,z = table.unpack(v.locations.shop.coords)
+			exports.ox_inventory:RegisterShop(k, {
+				name = v.label,
+				inventory = stashItems,
+				locations = {
+					vec3(x,y,z),
+				}
+			})
+		end
+	end
+	swapHook = exports.ox_inventory:registerHook('swapItems', function(payload)
+		for k,v in pairs(TokoCFG.Shops) do
+			if payload.fromInventory == k then
+				TriggerEvent('lynn_stockshop:refreshShop', k)
+			elseif payload.toInventory == k and tonumber(payload.fromInventory) ~= nil then
+				TriggerClientEvent('lynn_stockshop:setProductPrice', payload.fromInventory, k, payload.toSlot)
+			end
+		end
+	end, {})
+
+	buyHook = exports.ox_inventory:registerHook('buyItem', function(payload)
+		local metadata = payload.metadata
+		 if metadata?.shopData then
+			 local price = metadata.shopData.price
+			 local count = payload.count
+			 exports.ox_inventory:RemoveItem(metadata.shopData.shop, payload.itemName, payload.count)
+			 TriggerEvent('esx_addonaccount:getSharedAccount', 'society_'..metadata.shopData.shop, function(account)
+				 account.addMoney(price)
+			 end)
+		 end
+	 end, {})
+end)
+
+RegisterNetEvent('lynn_stockshop:refreshShop', function(shop)
+	Wait(250)
+	local items = exports.ox_inventory:GetInventoryItems(shop, false)
+	local stashItems = {}
+	for k,v in pairs(items) do
+		if v and v.name then
+			local metadata = v.metadata
+			if metadata?.shopData then
+				stashItems[#stashItems + 1] = { name = v.name, metadata = metadata, count = v.count, price = metadata.shopData.price }
+			end
+		end
+	end
+	exports.ox_inventory:RegisterShop(shop, {
+		name = TokoCFG.Shops[shop].label,
+		inventory = stashItems,
+		locations = {
+			TokoCFG.Shops[shop].locations.shop.coords,
+		}
+	})
+end)
+
+RegisterNetEvent('lynn_stockshop:setData', function(shop, slot, price)
+	local item = exports.ox_inventory:GetSlot(shop, slot)
+	if not item then return end
+	local metadata = item.metadata
+	metadata.shopData = {
+		shop = shop,
+		price = price
+	}
+	exports.ox_inventory:SetMetadata(shop, slot, metadata)
+	TriggerEvent('lynn_stockshop:refreshShop', shop)
 end)
